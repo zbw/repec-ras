d19cc10f8b6a63f07bb521e42fda82f8a0b810ff
Joachim Neubert <j.neubert@zbw.eu>
2016-12-13 13:27:26 +0000

    Add extract_dbpedia_repecid.pl

---
+++ bin/extract_dbpedia_repecid.pl
@@ -0,0 +1,28 @@
+#!/bin/env perl
+
+# extracts repecId statements from dbpedia
+
+use strict;
+use warnings;
+
+use URI::Escape;
+
+my $IN = $ARGV[0] || die "Usage: $0 {inputfilename}\n";
+
+open (my $in_fh, '<', $IN) or die "Could not open $IN: $!\n";
+
+while  (<$in_fh>) {
+  next unless m/repecId/;
+
+  m|(<http://dbpedia\.org/resource/(.*)?>) <http://dbpedia\.org/property/repecId> |;
+  next unless $1;
+
+  print $_;
+  my $dbpedia = $1;
+  my $page = $2;
+  $page =~ tr/_/ /;
+  $page = uri_escape($page);
+
+  print "$1 <http://xmlns.com/foaf/0.1/isPrimaryTopicOf> <https://en.wikipedia.org/wiki/$page> .\n";
+}
+
