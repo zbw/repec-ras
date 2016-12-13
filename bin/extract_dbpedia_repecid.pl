77252fd36b6876d6a57df76ef2ba1bca57197952
Joachim Neubert <j.neubert@zbw.eu>
2016-12-13 15:59:57 +0000

    Adapt to dbpedia version 2016-04

--- bin/extract_dbpedia_repecid.pl
+++ bin/extract_dbpedia_repecid.pl
@@ -18,11 +18,12 @@ while  (<$in_fh>) {
   next unless $1;
 
   print $_;
+
+  # create a pagelink statement (instead of reading it from dbpedia)
   my $dbpedia = $1;
   my $page = $2;
   $page =~ tr/_/ /;
   $page = uri_escape($page);
-
   print "$1 <http://xmlns.com/foaf/0.1/isPrimaryTopicOf> <https://en.wikipedia.org/wiki/$page> .\n";
 }
 
