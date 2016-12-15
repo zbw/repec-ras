c1f36bb2c555cc640455d4685164604171904344
Joachim Neubert <j.neubert@zbw.eu>
2016-12-15 14:26:49 +0000

    Add dbpedia_repec_wddirect.rq

--- bin/get_dbpedia.sh
+++ bin/get_dbpedia.sh
@@ -6,15 +6,31 @@
 # Adapt VERSION to the current dbpedia version
 VERSION="2016-04"
 
-SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/infobox_properties_mapped_en.ttl.bz2
 TARGET_DIR=../var/dbpedia/latest/rdf
-TARGET=$TARGET_DIR/infobox_properties_mapped_en.ttl
+
+cd $TARGET_DIR
+/bin/rm -f *.bz2
+
+SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/infobox_properties_mapped_en.ttl.bz2
+TARGET=infobox_properties_mapped_en.ttl
 
 if [ ! -f $TARGET ]; then
-  cd $TARGET_DIR
-  wget $SOURCE
-  bunzip2 *.bz2
+  wget --quiet $SOURCE
+  lbunzip2 --quiet *.bz2
   echo $TARGET created
 else
   echo $TARGET already exists
 fi
+
+SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/interlanguage_links_en.ttl.bz2
+TARGET=wikidata_links_en.ttl
+
+if [ ! -f $TARGET ]; then
+  wget --quiet $SOURCE
+  bzgrep -h 'sameAs> <http://www.wikidata' *.bz2 > $TARGET
+  rm *.bz2
+  echo $TARGET created
+else
+  echo $TARGET already exists
+fi
+
