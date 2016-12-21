0499d41fb45db8bee814c87c1e986a7effe75f41
Joachim Neubert <j.neubert@zbw.eu>
2016-12-21 19:12:28 +0000

    Use direct wd links instead of constructed sitelinks

--- bin/get_dbpedia.sh
+++ bin/get_dbpedia.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 # nbt, 13.13.2016
 
-# download and extract file with repecId assignments from dbpedia
+# download and extract files with repecId assignments from dbpedia
+# and with sameAs links to wikidata
 
 # Adapt VERSION to the current dbpedia version
 VERSION="2016-04"
 
-TARGET_DIR=../var/dbpedia/latest/rdf
+TARGET_DIR=../var/ras/latest/rdf
 
 cd $TARGET_DIR
 /bin/rm -f *.bz2
 
-SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/infobox_properties_mapped_en.ttl.bz2
-TARGET=infobox_properties_mapped_en.ttl
+FILE=infobox_properties_mapped_en.ttl.bz2
+SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/$FILE
+TARGET=dbpedia_repecid.ttl
 
 if [ ! -f $TARGET ]; then
   wget --quiet $SOURCE
-  lbunzip2 --quiet *.bz2
+  # extract repec id statements
+  bzgrep '<http://dbpedia.org/property/repecId>' $FILE > $TARGET
   echo $TARGET created
 else
   echo $TARGET already exists
 fi
 
-SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/interlanguage_links_en.ttl.bz2
-TARGET=wikidata_links_en.ttl
+FILE=interlanguage_links_en.ttl.bz2
+SOURCE=http://downloads.dbpedia.org/$VERSION/core-i18n/en/$FILE
+TARGET=dbpedia_wikidata_links.ttl
 
 if [ ! -f $TARGET ]; then
   wget --quiet $SOURCE
-  bzgrep -h 'sameAs> <http://www.wikidata' *.bz2 > $TARGET
-  rm *.bz2
+  # extract sameAs wikidata statements
+  bzgrep 'sameAs> <http://www.wikidata' $FILE > $TARGET
   echo $TARGET created
 else
   echo $TARGET already exists
