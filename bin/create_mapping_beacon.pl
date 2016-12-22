3df78f5cda8f82605b67efd6e7571527ba5e4cec
Joachim Neubert <j.neubert@zbw.eu>
2016-12-22 09:04:08 +0000

    Use STDOUT, create additional CSV file (temp)

--- bin/create_mapping_beacon.pl
+++ bin/create_mapping_beacon.pl
@@ -1,21 +1,24 @@
 #!/bin/env perl
+# nbt, 18.12.2016
 
-#
+# Create Beacon file for RePEc-ShortID properties of Wikidata
+# mapped from GND properties
 
 use strict;
 use warnings;
 use open ":encoding(utf8)";
 
 use Data::Dumper;
+use File::Slurp;
 use JSON qw'decode_json encode_json';
 use POSIX qw(strftime);
 use REST::Client;
 use URI::Escape;
-use WWW::Github::Files;
 
 # extract from Kims json file
-my $OUT_FN = '../var/pers_examples/example2/map/gnd_ras_mapping.example.txt';
+my $OUT_FN = '../var/pers_examples/example2/map/gnd_ras_mapping.example.csv';
 my $ENDPOINT   = 'http://172.16.10.102:3030/ebds/query';
+my $QUERY_FN = '../sparql/gnd_ras.rq';
 
 my $timestamp = strftime "%Y-%m-%d", localtime;
 my $beacon_header = <<"EOF";
@@ -34,36 +37,39 @@ EOF
 
 open( my $out_fh, '>', $OUT_FN )
   or die "Could not open $OUT_FN: $!\n";
-print $out_fh $beacon_header;
+print $out_fh "source, target, annotation\n";
 
-# queries are downloaded from a github repository
-my $gitfiles = WWW::Github::Files->new(
-  author => 'zbw',
-  resp   => 'repec_ras',
-  branch => 'master',
-);
+print $beacon_header;
 
 # initialize rest client
 my $client = REST::Client->new();
 
-# get query from github and encode it
-my $query = uri_escape( $gitfiles->get_file("/sparql/gnd_ras.rq") );
+# get query and encode it
+my $query = read_file($QUERY_FN);
+$query = uri_escape($query);
 
 # create GET url
 my $url = $ENDPOINT . '?query=' . $query;
 
 # execute the request (may also ask for 'text/csv') and write response to file
 $client->GET( $url, { 'Accept' => 'application/sparql-results+json' } );
-
-my $result_data = decode_json( $client->responseContent() );
+my $result_data;
+eval {
+  my $json = $client->responseContent();
+  $result_data = decode_json($json);
+};
+if ($@) {
+  die "Error parsing response: ", $client->responseContent(), "\n";
+}
 
 foreach my $entry ( @{ $result_data->{results}->{bindings} } ) {
 
-  print $out_fh $entry->{gndId}->{value}, '|', $entry->{rasLabel}->{value};
-  if ($entry->{affiliations}{value}) {
-    print $out_fh ' (', $entry->{affiliations}->{value}, ')';
-  }
-  print $out_fh '|', $entry->{rasId}->{value}, "\n";
+  print $entry->{gndId}->{value}, '|', $entry->{rasLabel}->{value};
+##  if ($entry->{affiliations}{value}) {
+##    print $out_fh ' (', $entry->{affiliations}->{value}, ')';
+##  }
+  print '|', $entry->{rasId}->{value}, "\n";
+  print $out_fh "$entry->{gndId}->{value}, $entry->{rasId}->{value}, \"$entry->{rasLabel}->{value}\"\n";
 }
 
 close $out_fh;
