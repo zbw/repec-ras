3a67eaa2830d019c58bfe397999e2d87471a9026
Joachim Neubert <j.neubert@zbw.eu>
2017-10-17 18:08:04 +0000

    Update stats

--- bin/update_stats.sh
+++ bin/update_stats.sh
@@ -5,7 +5,7 @@ for query in mapping_stats wd_mapping_statistics wd_top_mapping_statistics; do
   s-query.sh ebds ${query}.rq > results/${query}.json
 done
 
-EBDS_VERSION="ebds_2017-02-01"
+EBDS_VERSION="ebds_2017-10-10"
 
 cd /opt/sparql-queries/wikidata
 for query in count_ebds_pers; do
