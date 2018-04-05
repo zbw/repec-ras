b67f04153514b4f8a1478ce8c56983f6c844e9cb
Joachim Neubert <j.neubert@zbw.eu>
2018-04-05 17:17:19 +0000

    New statistics (with new graph names)

--- bin/update_stats.sh
+++ bin/update_stats.sh
@@ -5,7 +5,9 @@ for query in mapping_stats wd_mapping_statistics wd_top_mapping_statistics; do
   s-query.sh ebds ${query}.rq > results/${query}.json
 done
 
-EBDS_VERSION="ebds_2017-10-10"
+EBDS_VERSION="ebds_2018-03-05"
+
+echo Save EBDS result for version $EBDS_VERSION
 
 cd /opt/sparql-queries/wikidata
 for query in count_ebds_pers; do
